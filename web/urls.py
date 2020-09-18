e62841c0a117056663b0e72a3cd520d65668be81
m0tz <agustin_enriquez@hotmail.com>
2020-09-18 20:28:26 -0300

    clase2 first half

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from web.views import index, htmlpersonalizado, consumir_endpoint, respuesta_json
 
 
-urlpatterns = []
+urlpatterns = [
+    path("", index, name="index"),
+    path("holamundo", htmlpersonalizado, name="holamundo"),
+    path("endpoint", consumir_endpoint, name="endpoint"),
+    path("respuesta_json", respuesta_json, name="respuesta_json")
+]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,30 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import requests
+import json
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hola, mundo!")
+
+
+def htmlpersonalizado(request):
+    respuesta_html = "<h1>Hola, mundo personalizado!</h1>"
+    return HttpResponse(respuesta_html)
+
+
+def respuesta_json(request):
+    # Devuelve un json personalizado
+    mi_json = {"Nombre": "Agustin"}
+    return JsonResponse(json.dumps(mi_json), safe=False)
+
+def consumir_endpoint(request):
+    # Devuelve informacion sobre venta y compra a traves de una api externa.
+    endpoint = "https://api.recursospython.com/dollar"
+    respuesta = requests.get(endpoint)
+    precio_compra = respuesta.json()["buy_price"]
+    precio_venta = respuesta.json()["sale_price"]
+    respuesta_html = f"<p>Precio compra: {precio_compra}. Precio venta: {precio_venta}</p>"
+    return HttpResponse(respuesta_html)
