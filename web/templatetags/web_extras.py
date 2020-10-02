1ba0c17fbcaf9cefff3f08e95200326dba0194c3
m0tz <agustin_enriquez@hotmail.com>
2020-10-02 17:14:43 -0300

    templatetag edit

--- web/templatetags/web_extras.py
+++ web/templatetags/web_extras.py
@@ -9,7 +9,4 @@ def add_click_to_course_image(context):
     url = context['request'].path + "inscripcion"
     let_img = "let imagen_curso = document.querySelector(\'img[id=imagen-curso]\');"
     href = "imagen_curso.onclick = function(){ window.location.href = '" + url + "'}"
-    return (
-        f"{let_img}"
-        f"{href}"
-        )
+    return (f"{let_img}{href}")
