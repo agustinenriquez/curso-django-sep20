59c968e4651d9f13b0bd1a48628fad90aa2c4567
m0tz <agustin_enriquez@hotmail.com>
2020-10-13 00:01:31 -0300

    back to sqlite3

--- cursodjango/settings.py
+++ cursodjango/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'cursodjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'curso_django',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
